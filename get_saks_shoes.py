216cf85580cde470998cb630a42c6a5e32f50198
Herman Ng <yungvirginia03@gmail.com>
2015-03-01 01:04:16 -0800

    successful crawl for boots

--- get_saks_shoes.py
+++ get_saks_shoes.py
@@ -1,31 +1,65 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-# # base_url = 'http://www.saksfifthavenue.com/search/EndecaSearch.jsp?bmForm=endeca_search_form_one&bmFormID=kKYnHcK&bmUID=kKYnHcL&bmIsForm=true&bmPrevTemplate=%2Fmain%2FSectionPage.jsp&bmText=SearchString&SearchString=flats&submit-search=&bmSingle=N_Dim&N_Dim=0&bmHidden=Ntk&Ntk=Entire+Site&bmHidden=Ntx&Ntx=mode%2Bmatchpartialmax&bmHidden=prp8&prp8=t15&bmHidden=prp13&prp13=&bmHidden=sid&sid=14BBCA598131&bmHidden=FOLDER%3C%3Efolder_id&FOLDER%3C%3Efolder_id='
-base_url = 'http://www.saksfifthavenue.com/Christian-Louboutin/Shoes/shop/_/N-1z12vkzZ52k0s7'
 
-# # search_term = 'pumps'
-response = requests.get(base_url)
-# with open('/Users/heymanhn/Desktop/Christian.html','r') as s:
-#    saks = s.read()
-soup = BeautifulSoup(response.content, 'html.parser')
-img_lst = soup.select('div.image-container-large .pa-product-large')
+base_url = 'http://www.saksfifthavenue.com/Shoes/Boots/shop/_/N-52k0sa/Ne-6lvnb5?FOLDER%3C%3Efolder_id=2534374306624250&Nao='
+page_url = [base_url + str(i*60) for i in range(0,8)]
+
+# , base_url + '&Nao=180', base_url + '&Nao=360'
+
 
 counter = 0
-for item in img_lst:
-    print item
-    try:
-        link = item['src']
-        if link.startswith('http'):
-            image = requests.get(link).content
-            counter += 1
-            # f = open('/Users/heymanhn/Virginia/Zipfian/Capstone_Project/images/saks/flats%s.png' % str(counter)
-            #          , 'w')
-            f = open('/Users/heymanhn/Virginia/Zipfian/Capstone_Project/images/saks/louboutins%s.png' % str(counter)
-                     , 'w')
-            f.write(image)
-    except:
-        pass
+product_dict_name = {}
+product_dict_url  = {}
+
+for u in xrange(len(page_url)):
+# # Category = 'boots'
+    response = requests.get(page_url[u])
+    soup = BeautifulSoup(response.content, 'html.parser')
+    img_lst = soup.select('div.image-container-large .pa-product-large')
+    product_lst = soup.select('div.image-container-large a[href]')
+
+    for image_item in img_lst:
+        try:
+            image_link = image_item['src']
+            title = image_item['title']
+
+            if image_link.startswith('http'):
+                product_id = re.search(r"(?P<product_id>\d+)\_(\d+)",image_link).group("product_id")
+                product_dict_name [product_id] = title
+
+                image = requests.get(image_link).content
+
+                counter += 1
+                # f = open('/Users/heymanhn/Virginia/Zipfian/Capstone_Project/images/saks/flats%s.png' % str(counter)
+                #          , 'w')
+                f = open('/Users/heymanhn/Virginia/Zipfian/Capstone_Project/images/saks/boots_%s.png' % str(product_id)
+                         , 'w')
+                b = open('/Users/heymanhn/Virginia/Zipfian/Capstone_Project/images/saks/product_names.txt','w')
+        
+                f.write(image)
+                b.write(str(product_dict_name))
+        except:
+            pass
+
+    for product_item in product_lst:
+            try: 
+                
+                product_id2 = re.search(r"(\w+)\-(?P<product_id>\d+)",product_item['id']).group("product_id")
+                product_link = product_item['href']
+
+                if product_link.startswith('http'):
+                    product_dict_url [product_id2] = product_link
+
+                    w = open('/Users/heymanhn/Virginia/Zipfian/Capstone_Project/images/saks/product_links.txt', 'w')
+                    w.write(str(product_dict))
+            except:
+                pass
+
+
+re.search(r"(?P<product_id>\d+)\_(\d+)",image_link)
+
 
 
 
