08574656972054689d8200a246c6abc0c92656c1
Herman Ng <yungvirginia03@gmail.com>
2015-03-04 18:16:10 -0800

    feature eng module

--- Pipeline_FeatureEngineering.py
+++ Pipeline_FeatureEngineering.py
@@ -14,12 +14,23 @@ from Pipeline_CommonFunctions.py import clean_file_lst
 
 class Feature_Engineer(object):
 
-	def __init__(self, stand_img_directory):
+	def __init__(self, stand_img_directory, shape, target_shape=None):
 		"""
-		input: main directory containing all lable directories with standardized images
+		inputs: 
+		     main directory containing all lable directories with standardized images
+		     shape: tuple: current shape of images
+		     target_shape: tuple: target matrix size to run machine learning on.
+		                   default = current shape
 		output: feature_matrix
 		"""
 		self.stand_img_directory = stand_img_directory
+		self.shape = shape
+		target_shape = None
+
+	def flatten(self, img_arr):
+		flat_img_arr = img_arr.flatten()[np.newaxis, :]
+
+		return flat_img_arr
 
 	def rescaling(self, flat_img_arr):
 		scaler = StandardScaler()
@@ -27,11 +38,6 @@ class Feature_Engineer(object):
 
 		return rescaled_img_arr
 
-
-	def flatten(self, img_arr):
-		flat_img_arr = img_arr.flatten()[np.newaxis, :]
-
-		return flat_img_arr
 
 	def sobel_filter(self, img_arr):
 		"""
@@ -61,17 +67,21 @@ class Feature_Engineer(object):
 		main feature preprocessing done
 		output: feature_matrix
 		"""
-		feature_matrix = []
+	
 		clean_stand_img_directory_lst = clean_file_lst(os.listdir(self.stand_img_directory), jpg=False)
 		for subdir in clean_stand_img_directory_lst:
 			subdir_path = os.path.join(self.stand_img_directory, subdir)
 			clean_subdir_lst = self.clean_file_lst(subdir_path, jpg=True)
-			for img_file in clean_subdir_lst:
+			for i, img_file in enumerate(clean_subdir_lst):
+				img_mat = np.empty([len(clean_subdir_lst[i]), self.shape[0]*self.shape[1]])
 				img_arr = io.imread('shoes/%s' % file_name)
 				flat_img_arr = self.flatten(img_arr)
-				rescaled_img = self.rescaling(flat_img_arr)
-				sobel = self.sobel_filter(rescaled_img)
-				feature_matrix.append(sobel)
+				img_mat[i] = flat_img_arr
+				print img_mat.shape
+			rescaled_img_mat = self.rescaling(img_mat)
+			sobel_mat = self.sobel_filter(rescaled_img_mat)
+			filtered_img_mat = sobel_mat
+			feature_matrix = filtered_img_mat
 
 		return feature_matrix
 
@@ -79,6 +89,6 @@ class Feature_Engineer(object):
 if __name__ == '__main__':
 	dir_path = '/Users/heymanhn/Virginia/Zipfian/Capstone_Project/Image_New'
 	full_dir_path = os.path.join(dir_path, 'Output_Images')
-	fm = Feature_Engineer(full_dir_path)
+	fm = Feature_Engineer(full_dir_path, (28, 28))
 
 
