14fef1c1de11571d51cab671ece043ee2db572fa
Herman Ng <heymanhn@gmail.com>
2015-04-06 01:12:53 -0700

    Functional scraper to get the item URLs

--- anastasia/scraper.js
+++ anastasia/scraper.js
@@ -2,26 +2,48 @@ var _ = require('underscore'),
     cheerio = require('cheerio'),
     express = require('express'),
     http = require('http'),
-    mongojs = require('mongojs');
+    mongojs = require('mongojs'),
+    request = require('request');
 
-var BARNEYS_OPTS = {
-    host: 'www.barneys.com',
-    method: 'GET',
-    path: '/barneys-new-york/women/shoes/'
+var BARNEYS_BASE_URL = 'http://www.barneys.com/barneys-new-york/women/shoes';
+var barneysItems = [];
+
+var scrapeURL = function(url) {
+    console.log('Scraping ' + url);
+    request.get(url).on('response', function(res) {
+        var data = '';
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            data += chunk;
+        });
+        res.on('end', function() {
+            getBarneysItems(data);
+        });
+    });
 };
 
-var req = http.get(BARNEYS_OPTS, function(res) {
-    res.setEncoding('utf8');
-    res.on('data', function(chunk) {
-        console.log('BODYPART: ' + chunk);
+var getBarneysItems = function(html) {
+    $ = cheerio.load(html);
+    $('#search-result-items .thumb-link').each(function() {
+        var currentItem = $(this).attr('href');
+        //console.log('Adding item ' + currentItem);
+        barneysItems.push(currentItem);
     });
 
-    res.on('end', function() {
-        console.log('everything crawled');
-    });
-});
+    // Recursively scrape the next page until we've reached the end
+    var nextPage = $('.pagination .page-next.active').attr('href');
+    if (!nextPage) {
+        console.log("Total of " + barneysItems.length + " shoes scraped");
 
-req.on('error', function(error) {
-    console.log('Got error: ' + error.message);
-});
+        // TO-DO: Scrape the individual items and save into MongoDB
+        process.exit(0);
+    } else {
+        scrapeURL(nextPage);
+    }
+};
 
+var startScrape = function() {
+    scrapeURL(BARNEYS_BASE_URL);
+};
+
+startScrape();
